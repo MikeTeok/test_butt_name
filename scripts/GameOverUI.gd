ab472b31f48e1a221c4e230170f744a282350012
bingxuan3133 <bensoncbx@gmail.com>
2021-08-28 11:00:19 +0800

    Added SaveState.gd to save best score
    Game data path: (Windows)
    C:\Users\user\AppData\Roaming\Godot\app_userdata\test_butt

--- scripts/GameOverUI.gd
+++ scripts/GameOverUI.gd
@@ -7,13 +7,19 @@ onready var home_button = $CenterContainer/VBoxContainer/HBoxContainer/CenterCon
 onready var board = $Board
 signal restartRequest
 onready var theme_mode = "Light mode" setget set_theme
+var best = 0
 
 func _ready():
 	set_theme(theme_mode)
 
 func update_scoreboard(score):
 	scoreLabel.set_text("SCORE\n" + String(score))
-	hiScoreLabel.set_text("BEST\n" + String(score))
+	if (score > best):
+		best = score
+		hiScoreLabel.set_text("NEW BEST\n" + String(best))
+	else:
+		hiScoreLabel.set_text("BEST\n" + String(best))
+	SaveState.save_game()
 
 func _on_RestartButton_pressed():
 	emit_signal("restartRequest")
@@ -26,3 +32,13 @@ func set_theme(new_theme):
 		board.modulate = Color("2A2A2A")
 	else:
 		board.modulate = Color("e1e6ec")
+
+func get_save_stats():
+	return {
+		'name' : get_name(),
+		'parent' : get_parent().get_path(),
+		'best' : best
+	}
+
+func load_save_stats(stats):
+	self.best = stats.best
