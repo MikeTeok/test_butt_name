d0bf2650cb5e83d47d62548e627bbf3cefefb411
MikeTeok <miketeok94@gmail.com>
2021-09-04 18:39:18 +0800

    added piak
    added piak

--- scripts/HandSlapEffect.gd
+++ scripts/HandSlapEffect.gd
@@ -1,8 +1,19 @@
 extends AnimatedSprite
 
+onready var piak = $piak
+
 func _ready():
 	frame = 0
+	piak.visible = false
 
 func _on_HandSlapEffect_animation_finished():
 	stop()
 	frame = 0
+
+
+func _on_HandSlapEffect_frame_changed():
+	if frame == 2:
+		piak.visible = true
+	
+	if frame == 7:
+		piak.visible = false
